997d12b897eed81d52045093a222425092d5643b
MaxDev-9 <amax98110@gmail.com>
2026-05-03 22:07:08 +0300

    fix: use UseStaticFiles instead of MapStaticAssets for .NET 8 compatibility

--- Program.cs
+++ Program.cs
@@ -47,11 +47,10 @@ app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
